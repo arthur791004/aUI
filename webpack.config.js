e25743707d9ef27bf6b04c487f3be2e79bbdddfb
arthur <arthur791004>
2017-08-06 10:01:03 +0000

    init

---
+++ webpack.config.js
@@ -0,0 +1,45 @@
+const path = require('path');
+const webpack = require('webpack');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+const plugins = [
+  new webpack.NoEmitOnErrorsPlugin(),
+  new HtmlWebpackPlugin({
+    template: path.join(process.cwd(), 'app/index.html'),
+    inject: true,
+  }),
+];
+
+module.exports = {
+  entry: [
+    path.join(process.cwd(), 'app/app.js'),
+  ],
+  output: {
+    path: path.resolve(process.cwd(), 'build'),
+    filename:'[name].js',
+    chunkFilename: '[name].chunk.js',
+  },
+  module: {
+    rules: [{
+      test: /\.js$/,
+      exclude: /node_modules/,
+      use: {
+        loader: 'babel-loader',
+      },
+    }],
+  },
+  plugins,
+  resolve: {
+  },
+  devtool: 'cheap-module-eval-source-map',
+  performance: {
+    hints: false,
+  },
+  devServer: {
+    port: '8080',
+    contentBase: './build',
+    host: '0.0.0.0',
+    disableHostCheck: true,
+  },
+};
+
