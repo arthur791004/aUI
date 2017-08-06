6e8349b6e3bb86f6cb0346483be2906c20b05d4c
arthur <arthur791004>
2017-08-06 10:01:03 +0000

    init

---
+++ app/app.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const render = () => {
+  ReactDOM.render(
+    <div>a UI</div>,
+    document.getElementById('app')
+  );
+};
+
+render();
