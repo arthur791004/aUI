325d79ba1c2dce1403b254f653303adde4048439
arthur791004 <arthur791004>
2017-08-08 16:03:06 +0000

    Add `TextField` using `styled-components` and `polished`

--- app/containers/App/index.js
+++ app/containers/App/index.js
@@ -1,7 +1,17 @@
 import React from 'react';
+import styled from 'styled-components';
+import TextField from '@/components/TextField';
+
+const AppWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+`;
 
 const App = () => (
-  <div>aUI Demo Page</div>
+  <AppWrapper>
+    <div>aUI Demo Page</div>
+    <TextField label="label" />
+  </AppWrapper>
 );
 
 export default App;
